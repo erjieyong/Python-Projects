85a8bdb12efe2789509cd707f8fdb435453d08a9
Er Jie Yong <erjieyong@gmail.com>
2022-09-26 10:13:17 +0800

    update background picture

--- Ames_House_Price_Prediction/streamlit_app.py
+++ Ames_House_Price_Prediction/streamlit_app.py
@@ -580,7 +580,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://images.unsplash.com/photo-1557443611-e64d90d03484?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80");
+             background-image: url("./bg.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
